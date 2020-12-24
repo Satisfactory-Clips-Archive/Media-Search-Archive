046084cd0814e073929b1820167a0ffffef9b72e
SignpostMarv <me@signpostmarv.name>
2020-12-24 03:48:44 +0000

    add synonyms to search

--- app/lunr-docs-prebuild-index.js
+++ app/lunr-docs-prebuild-index.js
@@ -4,8 +4,29 @@ const documents = Object.entries(require('./lunr-docs-preload.json')).map(
 		return e[1];
 	}
 );
+const synonyms = require('./synonyms.json');
+const synonym_keys = Object.keys(synonyms);
 
 const stdout = process.stdout;
+
+function aliasSatisfactoryVocabulary(builder) {
+	const pipeline = (token) => {
+		const lowercase = token.toString().toLowerCase();
+
+		if (synonym_keys.includes(synonyms)) {
+			return token.update(() => {
+				return synonyms[lowercase];
+			});
+		}
+
+		return token;
+	};
+
+	lunr.Pipeline.registerFunction(pipeline, 'aliasSatisfactoryVocabulary');
+
+	builder.pipeline.before(lunr.stemmer, pipeline);
+	builder.searchPipeline.before(lunr.stemmer, pipeline);
+}
 
 const index = lunr(function () {
 	this.ref('id');
@@ -20,6 +41,7 @@ const index = lunr(function () {
 	this.field('topics');
 	*/
 	this.field('quotes');
+	this.use(aliasSatisfactoryVocabulary);
 
 	documents.forEach((doc) => {
 		this.add(doc);
