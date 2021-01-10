5bcb8b0dbafa08e51d490220d6d1308921f1f802
SignpostMarv <me@signpostmarv.name>
2021-01-10 04:42:31 +0000

    ingest the markdown from twitch-clip-notes

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = {
+	dir: {
+		data: '../../11ty/data',
+		input: './twitch-clip-notes/coffeestainstudiosdevs',
+		layouts: '../../11ty/layouts',
+		output: './src',
+	},
+};
--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,17 @@ const zopfli = require('gulp-zopfli-green');
 const postcss = require('gulp-postcss');
 const postcss_plugins = {
 	nested: require('postcss-nested'),
+	import: require('postcss-import'),
 };
 const cssnano = require('cssnano');
 const rename = require('gulp-rename');
+const replace = require('gulp-replace');
+const sitemap = require('gulp-sitemap');
 
 gulp.task('css', () => {
-	return gulp.src('./src/**/*.postcss').pipe(
+	return gulp.src('./src/{index,browse}.postcss').pipe(
 		postcss([
+			postcss_plugins.import(),
 			postcss_plugins.nested(),
 			cssnano({
 				cssDeclarationSorter: 'concentric-css',
@@ -44,7 +48,7 @@ gulp.task('rev', () => {
 
 gulp.task('brotli', () => {
 	return gulp.src(
-		'./tmp/**/*.{js,json,css,html}'
+		'./tmp/**/*.{js,json,css,html,xml}'
 	).pipe(
 		newer({
 			dest: './tmp/',
@@ -61,7 +65,7 @@ gulp.task('brotli', () => {
 
 gulp.task('zopfli', () => {
 	return gulp.src(
-		'./tmp/**/*.{js,json,css,html}'
+		'./tmp/**/*.{js,json,css,html,xml}'
 	).pipe(
 		newer({
 			dest: './tmp/',
@@ -103,12 +107,56 @@ gulp.task('html', () => {
 	);
 });
 
+gulp.task('html-11ty', () => {
+	return gulp.src(
+		'./src/satisfactory/**/*.html'
+	).pipe(
+		newer('./tmp/satisfactory/')
+	).pipe(
+		rev_replace({
+			manifest: gulp.src('./tmp/asset.manifest'),
+		})
+	).pipe(
+		replace('.md', '/')
+	).pipe(
+		replace('"satisfactory/', '"/satisfactory/')
+	).pipe(
+		replace('"./topics/', '"/satisfactory/topics/')
+	).pipe(
+		replace(/"(?:\.\.\/)+topics\/?/g, '"/satisfactory/topics/')
+	).pipe(
+		htmlmin({
+			collapseInlineTagWhitespace: true,
+			collapseWhitespace: true,
+			minifyCSS: true,
+			minifyJs: true,
+			removeAttributeQuotes: true,
+			removeComments: true,
+			useShortDoctype: true,
+		})
+	).pipe(
+		gulp.dest('./tmp/satisfactory/')
+	);
+});
+
 gulp.task('sync-favicon', () => {
 	return gulp.src('./src/favicon.ico').pipe(gulp.dest('./dist/'));
 });
 
+gulp.task('sitemap', () => {
+	return gulp.src(
+		'./tmp/**/*.html'
+	).pipe(
+		sitemap({
+			siteUrl: 'https://clips.satisfactory.signpostmarv.name/'
+		})
+	).pipe(
+		gulp.dest('./tmp')
+	)
+});
+
 gulp.task('sync-tmp-to-store', () => {
-	return gulp.src('./tmp/**/*.{js,css,html,json,gz,br}').pipe(
+	return gulp.src('./tmp/**/*.{js,css,html,json,xml,gz,br}').pipe(
 		changed(
 			'./dist/',
 			{
@@ -125,7 +173,11 @@ gulp.task('sync-tmp-to-store', () => {
 gulp.task('build', gulp.series(
 	'css',
 	'rev',
+	gulp.parallel(
 	'html',
+		'html-11ty'
+	),
+	'sitemap',
 	gulp.parallel(
 		'zopfli',
 		'brotli'
