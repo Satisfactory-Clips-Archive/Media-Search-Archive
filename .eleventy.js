2ef1ea7b24dc8c675eb22bc7e6e4a3d458c46a6c
SignpostMarv <me@signpostmarv.name>
2021-01-13 04:07:19 +0000

    updating site generation

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = {
 		data: '../../../11ty/data',
 		input: './twitch-clip-notes/coffeestainstudiosdevs/satisfactory',
 		layouts: '../../../11ty/layouts',
+		includes: '../../../11ty/includes',
 		output: './src',
 	},
 };
