c40c5031c6cc35533a10f9f487db7aa14c04c3ab
SignpostMarv <me@signpostmarv.name>
2021-01-21 08:26:04 +0000

    adjusting indentation

--- 11ty/data/jsonld.js
+++ 11ty/data/jsonld.js
@@ -22,18 +22,18 @@ module.exports = () => {
 			{}
 		),
 		{
-	"/": [
-		{
-			"@context": "https://schema.org",
-			"@type": "WebSite",
-			"url": "https://clips.satisfactory.signpostmarv.name/",
-			"potentialAction": {
-				"@type": "SearchAction",
-				"target": "https://clips.satisfactory.signpostmarv.name/search?q={search_term_string}",
-				"query-input": "required name=search_term_string"
-			}
-		}
-	]
+			"/": [
+				{
+					"@context": "https://schema.org",
+					"@type": "WebSite",
+					"url": "https://clips.satisfactory.signpostmarv.name/",
+					"potentialAction": {
+						"@type": "SearchAction",
+						"target": "https://clips.satisfactory.signpostmarv.name/search?q={search_term_string}",
+						"query-input": "required name=search_term_string"
+					}
+				}
+			],
 		}
 	);
 
