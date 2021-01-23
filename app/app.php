2066be22dba04e9a1c37a631ab3898ff29515568
SignpostMarv <me@signpostmarv.name>
2021-01-23 05:53:34 +0000

    remove unused function import

--- app/app.php
+++ app/app.php
@@ -13,7 +13,6 @@ use function array_keys;
 use function array_map;
 use function array_merge;
 use function array_merge_recursive;
-use function array_pop;
 use function array_reverse;
 use function array_unique;
 use function array_values;
