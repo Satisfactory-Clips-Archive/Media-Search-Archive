d3cf73ca14a50e3bd3079741e71ac48bfe6983cd
SignpostMarv <me@signpostmarv.name>
2021-02-15 10:44:18 +0000

    move filter to central location

---
+++ app/src/Filtering.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author SignpostMarv
+ */
+declare(strict_types=1);
+
+namespace SignpostMarv\VideoClipNotes;
+
+class Filtering
+{
+	/**
+	 * @param array{
+	 *	duplicates:list<string>,
+	 *	replaces:list<string>,
+	 *	seealso:list<string>,
+	 *	replacedby?:string,
+	 *	duplicatedby?:string
+	 * } $maybe
+	 */
+	public function QuestionDataNoReferences(array $maybe) : bool
+	{
+		return
+			count($maybe['duplicates'] ?? []) < 1
+			&& count($maybe['replaces'] ?? []) < 1
+			&& count($maybe['seealso'] ?? []) < 1
+			&& ! isset($maybe['replacedby'])
+			&& ! isset($maybe['duplicatedby'])
+		;
+	}
+}
