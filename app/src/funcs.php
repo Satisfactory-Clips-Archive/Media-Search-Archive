fee703238a4c8da3b16ec23811cdfeef666a36c4
SignpostMarv <me@signpostmarv.name>
2021-02-22 20:11:37 +0000

    adding february 6th, 2019 q&a livestream

--- app/src/funcs.php
+++ app/src/funcs.php
@@ -1604,11 +1604,10 @@ function process_dated_csv(
 		array_filter(
 			$externals_csv,
 			static function (int $k) use ($data) : bool {
-				if ( ! isset($data['skip'])) {
-					return false;
-				}
-
-				return ! $data['skip'][$k];
+				return
+					isset($data['skip'][$k])
+						? ( ! $data['skip'][$k])
+						: isset($data['topics'][$k]);
 			},
 			ARRAY_FILTER_USE_KEY
 		)
