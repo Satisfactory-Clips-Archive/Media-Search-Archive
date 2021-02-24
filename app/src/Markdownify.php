e2954c97d71bd00e7895d822f6e5b64ac17857ff
SignpostMarv <me@signpostmarv.name>
2021-02-24 16:25:02 +0000

    remove echo statements only after refactoring has been completed

--- app/src/Markdownify.php
+++ app/src/Markdownify.php
@@ -7,8 +7,6 @@ declare(strict_types=1);
 namespace SignpostMarv\VideoClipNotes;
 
 use function count;
-use function ob_get_contents;
-use function ob_start;
 use function sprintf;
 use function preg_replace;
 
@@ -50,33 +48,32 @@ class Markdownify
 		$video_part_info = cached_part_continued()[$video_id];
 		$video_other_parts = other_video_parts($video_id, $include_self);
 
-		ob_start();
-
-		echo "\n",
-			'<details>',
-			"\n",
-			'<summary>';
+		$out = "\n"
+			. '<details>'
+			. "\n"
+			. '<summary>'
+		;
 
 		if (count($video_other_parts) > 2) {
-			echo sprintf(
+			$out .= sprintf(
 				'This video is part of a series of %s videos.',
 				count($video_other_parts)
 			);
 		} elseif (null !== $video_part_info['previous']) {
-			echo 'This video is a continuation of a previous video';
+			$out .= 'This video is a continuation of a previous video';
 		} else {
-			echo 'This video continues in another video';
+			$out .= 'This video continues in another video';
 		}
 
-		echo '</summary>', "\n\n";
+		$out .= '</summary>' . "\n\n";
 
 		if (count($video_other_parts) > 2) {
 			$video_other_parts = other_video_parts($video_id, false);
 		}
 
 		foreach ($video_other_parts as $other_video_id) {
-			echo '* ',
-				preg_replace('/\.md\)/', ')', str_replace(
+			$out .= '* '
+				. preg_replace('/\.md\)/', ')', str_replace(
 					'./',
 					'https://archive.satisfactory.video/',
 					maybe_transcript_link_and_video_url(
@@ -89,13 +86,13 @@ class Markdownify
 							. $this->injected->cache['playlistItems'][$other_video_id][1]
 						)
 					)
-				)),
-				"\n"
+				))
+				. "\n"
 			;
 		}
 
-		echo '</details>', "\n";
+		$out .= '</details>' . "\n";
 
-		return ob_get_clean();
+		return $out;
 	}
 }
