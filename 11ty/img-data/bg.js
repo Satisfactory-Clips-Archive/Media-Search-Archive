40282478261fe04fe74d0d46f0ddd46f365be2d0
SignpostMarv <me@signpostmarv.name>
2021-07-31 15:30:57 +0100

    amend image building with textured background

---
+++ 11ty/img-data/bg.js
@@ -0,0 +1,16 @@
+const {
+	readFileSync,
+} = require('fs');
+
+const bgs = {
+	blue_graph_paper: `data:image/webp;base64,${
+		readFileSync(
+			`${__dirname}/../../images-ref/banner-1571861/blue-graph-paper.webp`,
+			{
+				encoding: 'base64'
+			}
+		)
+	}`,
+};
+
+module.exports = bgs;
