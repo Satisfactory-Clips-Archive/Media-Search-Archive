d6f31cea98c46a840a111a624c03a1d4b4306681
SignpostMarv <me@signpostmarv.name>
2023-04-14 03:06:38 +0100

    refactoring source of sub-questions

--- app/src/SubQuestions.php
+++ app/src/SubQuestions.php
@@ -17,6 +17,67 @@ class SubQuestions extends AbstractQuestions
 		parent::__construct($injected->sorting);
 	}
 
+	/**
+	 * @param list<string> $ignore_these_video_ids
+	 */
+	public function obtain_sub_question_ids(array $ignore_these_video_ids): array
+	{
+		/** @var list<string> */
+		$sub_question_ids = [];
+
+		foreach ($this->injected->all_video_ids() as $video_id)
+		{
+			if ($this->string_is_probably_question((string) $this->injected->determine_video_title($video_id))) {
+				continue;
+			}
+
+			if (in_array($video_id, $ignore_these_video_ids, true)) {
+				continue;
+			}
+
+			$chapters = array_values(array_filter(
+				array_map(
+					static function (string $timestamped_line): string {
+						return trim(preg_replace('/ https?:\/\/.+/', '', $timestamped_line));
+					},
+					array_filter(
+						array_map(
+							'trim',
+							explode("\n", (string) $this->injected->determine_video_description($video_id))
+						),
+						static function (string $maybe) : bool {
+							return '' !== $maybe && preg_match('/^(\d+:)+\d{2} .+/', $maybe);
+						}
+					)
+				),
+				function (string $maybe) : bool {
+					return $this->string_is_probably_question($maybe);
+				}
+			));
+
+			if (count($chapters) < 1) {
+				continue;
+			}
+
+			foreach ($chapters as $chapter) {
+				$chapter_parts = explode(' ', $chapter);
+				$timestamp = array_shift($chapter_parts);
+
+				$time_parts = array_reverse(explode(':', $timestamp));
+
+				$seconds = 0;
+
+				foreach ($time_parts as $offset => $time_part) {
+					$seconds += ((int)$time_part * (60 ** $offset));
+				}
+
+				$sub_question_ids[] = $video_id . '#' . $timestamp;
+			}
+		}
+
+		return $sub_question_ids;
+	}
+
 	public function update(): void
 	{
 		$out = json_decode(
@@ -26,12 +87,10 @@ class SubQuestions extends AbstractQuestions
 			JSON_THROW_ON_ERROR
 		);
 
+		$sub_question_ids = $this->obtain_sub_question_ids([]);
+
 		foreach ($this->injected->all_video_ids() as $video_id)
 		{
-			if ($this->string_is_probably_question((string) $this->injected->determine_video_title($video_id))) {
-				continue;
-			}
-
 			$chapters = array_values(array_filter(
 				array_map(
 					static function (string $timestamped_line): string {
@@ -72,6 +131,10 @@ class SubQuestions extends AbstractQuestions
 				}
 
 				$chapter_id = $video_id . '#' . $timestamp;
+
+				if ( ! in_array($chapter_id, $sub_question_ids, true)) {
+					continue;
+				}
 
 				if (isset($out[$chapter_id])) {
 					$stub = $out[$chapter_id];
